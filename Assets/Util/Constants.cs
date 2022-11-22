d4efd276d8be3044b117f277870b26353b875f73
Quinn Bowers <q.bowers@outlook.com>
2022-11-22 02:57:49 -0500

    Copy Menus and Control Flow

--- Assets/Util/Constants.cs
+++ Assets/Util/Constants.cs
@@ -7,7 +7,7 @@ public class Constants {
     public static string[] Menus = new string[]{StartMenuScene, PauseMenuScene, GameOverMenuScene, WinMenuScene};
 
     public const string LevelSystemsScene = "LevelSystems";
-    public const string LevelOne = "Level_Jump";
+    public const string LevelOne = "Level 2";
 
     public const string mouseAimBinding = "FPS_player";
     public const string keyboardAimBinding = "Platformer_player";
