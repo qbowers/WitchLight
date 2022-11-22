2e67781f2e56c8abcb96e7f0f0a16678032fcaf4
Quinn Bowers <q.bowers@outlook.com>
2022-11-22 01:48:33 -0500

    copy util scripts

---
+++ Assets/Util/Constants.cs
@@ -0,0 +1,14 @@
+public class Constants {
+    public const string StartMenuScene = "Start Menu";
+    public const string PauseMenuScene = "Pause Menu";
+    public const string GameOverMenuScene = "Game Over";
+    public const string WinMenuScene = "Win Menu";
+
+    public static string[] Menus = new string[]{StartMenuScene, PauseMenuScene, GameOverMenuScene, WinMenuScene};
+
+    public const string LevelSystemsScene = "LevelSystems";
+    public const string LevelOne = "Level_Jump";
+
+    public const string mouseAimBinding = "FPS_player";
+    public const string keyboardAimBinding = "Platformer_player";
+}
