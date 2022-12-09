c4d95cbc088392ededbfb7f127484a02622e3f52
Hali Huang <halihuang22@gmail.com>
2022-12-09 17:41:58 -0500

    rework menu

---
+++ Assets/Menus/GetCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCamera : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.worldCamera = CoreManager.instance.camera.GetComponent<Camera>();
+    }
+}
